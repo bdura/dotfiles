5744aa558032aaf8c429c5cb6d7d5122661945ae
Basile Dura <basile@bdura.me>
2024-11-18 22:50:44 +0100

    feat(nvim): add question comment

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -21,6 +21,7 @@ return {
       WARN = { icon = ' ', color = 'warning', alt = { 'WARNING', 'XXX' } },
       PERF = { icon = ' ', alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' } },
       NOTE = { icon = ' ', color = 'hint', alt = { 'INFO' } },
+      QUESTION = { icon = ' ', color = 'warning' },
       TEST = { icon = '⏲ ', color = 'test', alt = { 'TESTING', 'PASSED', 'FAILED' } },
     },
   },
