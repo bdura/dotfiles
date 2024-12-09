e83afa36785cfdda80a4e7a464ffd699e345489f
Basile Dura <basile@bdura.me>
2024-12-09 15:48:12 +0100

    chore: allow stow on DB laptop

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -10,7 +10,14 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if prefer nvim-web-devicons
   config = function()
     local oil = require('oil')
-    oil.setup()
+    oil.setup({
+      view_options = {
+        show_hidden = true,
+        is_always_hidden = function(name, _)
+          return name == '..' or name == '.git'
+        end,
+      },
+    })
     vim.keymap.set('n', '-', oil.toggle_float, {})
   end,
 }
