3c034df9beae2af39caf9acc137d8ef96799d87c
Basile Dura <basile@bdura.me>
2025-02-19 22:35:00 +0100

    feat: install lazygit & pre-commit via nix

--- .config/nix/flake.nix
+++ .config/nix/flake.nix
@@ -14,12 +14,14 @@
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages = with pkgs; [
-        neovim
-        mkalias
-        ripgrep
         fzf
-        tmux
+        lazygit
+        mkalias
+        neovim
+        pre-commit
+        ripgrep
         starship
+        tmux
         wezterm
       ];
 
