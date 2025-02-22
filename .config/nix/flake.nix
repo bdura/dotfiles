3bff61cd2dedcda58c32322132be9d9f72a35984
Basile Dura <basile@bdura.me>
2025-02-22 03:26:05 +0100

    feat: add karabiner-elements service

--- .config/nix/flake.nix
+++ .config/nix/flake.nix
@@ -143,12 +143,30 @@
             dock.mru-spaces = false;
           };
 
+          # Enable Karabiner for key remapping
+          services.karabiner-elements.enable = true;
+
           # Enable yabai & skhd
           services.yabai.enable = true;
           services.skhd.enable = true;
 
           # Allow unfree apps:
           nixpkgs.config.allowUnfree = true;
+
+          nixpkgs.overlays = [
+            # Fixing a bug with karabiner-elements installation
+            # See <https://github.com/LnL7/nix-darwin/issues/1041>
+            (self: super: {
+              karabiner-elements = super.karabiner-elements.overrideAttrs (old: {
+                version = "14.13.0";
+
+                src = super.fetchurl {
+                  inherit (old.src) url;
+                  hash = "sha256-gmJwoht/Tfm5qMecmq1N6PSAIfWOqsvuHU8VDJY8bLw=";
+                };
+              });
+            })
+          ];
 
           # The platform the configuration will be used on.
           nixpkgs.hostPlatform = "aarch64-darwin";
