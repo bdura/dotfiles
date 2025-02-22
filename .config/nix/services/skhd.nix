adbe935729f9ff0f2e86e4781b9bd6ddd86022ae
Basile Dura <basile@bdura.me>
2025-02-22 15:50:46 +0100

    docs: document yabai & skhd, including difficulties with skhd service

--- .config/nix/services/skhd.nix
+++ .config/nix/services/skhd.nix
@@ -1,3 +1,14 @@
+# skhd is a hotkey deamon for macOS.
+#
+# Refer to the github page: <https://github.com/koekeishiya/skhd>.
+#
+# skhd supports hot reloading, so the configuration is in `.config` directly.
+#
+# I have been unable to make it work as a launchd service, whether through nix or not.
+# Calling it directly works fine, though.
+#
+# The issue may stem from an inability to listen for hotkeys when launched as a service?
+
 {
   pkgs,
   ...
