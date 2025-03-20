2c5e74e9aa9301d931a67b7c7c1c7f9e831dc6fa
Basile Dura <basile@bdura.me>
2025-03-20 15:24:21 +0100

    feat: add flash options

---
+++ .config/nvim/lua/plugins/flash.lua
@@ -0,0 +1,29 @@
+return {
+  'folke/flash.nvim',
+  event = 'VeryLazy',
+  vscode = true,
+  ---@type Flash.Config
+  opts = {
+    modes = {
+      char = {
+        jump_labels = true,
+      },
+    },
+    jump = {
+      autojump = true,
+    },
+  },
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    {"<leader>j", mode = {"n"}, function() require("flash").jump({
+  search = { mode = "search", max_length = 0 },
+  label = { after = { 0, 0 } },
+  pattern = "^"
+}) end, desc = "Flash line"},
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
+}
