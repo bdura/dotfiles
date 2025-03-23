0e77c640734e7cad9156286f4a89cf02fb605bb2
Basile Dura <basile@bdura.me>
2025-03-23 16:21:55 +0100

    feat: activated driver

--- .config/nix-darwin/services/kanata.nix
+++ .config/nix-darwin/services/kanata.nix
@@ -35,93 +35,34 @@ let
     )
   '';
   configFile = pkgs.writeScript "kanata.kbd" config;
-  parentAppDir = "/Applications";
 in
 {
   config = {
     environment.systemPackages = [ pkgs.kanata ];
 
-    # system.activationScripts.preActivation.text = ''
-    #   rm -rf ${parentAppDir}
-    #   mkdir -p ${parentAppDir}
-    #   # Kernel extensions must reside inside of /Applications, they cannot be symlinks
-    #   cp -r ${karabiner.driver}/Applications/.Karabiner-VirtualHIDDevice-Manager.app ${parentAppDir}
-    # '';
-
-    system.activationScripts.postActivation.text = ''
-      echo "attempt to activate karabiner system extension and start daemons" >&2
-      launchctl unload /Library/LaunchDaemons/org.nixos.start_karabiner_daemons.plist
-      launchctl load -w /Library/LaunchDaemons/org.nixos.start_karabiner_daemons.plist
-    '';
-
-    # We need the karabiner_grabber and karabiner_observer daemons to run after the
-    # Nix Store has been mounted, but we can't use wait4path as they need to be
-    # executed directly for the Input Monitoring permission. We also want these
-    # daemons to auto restart but if they start up without the Nix Store they will
-    # refuse to run again until they've been unloaded and loaded back in so we can
-    # use a helper daemon to start them. We also only want to run the daemons after
-    # the system extension is activated, so we can call activate from the manager
-    # which will block until the system extension is activated.
-    launchd.daemons.start_karabiner_daemons = {
-      script = ''
-        /Applications/.Karabiner-VirtualHIDDevice-Manager.app/Contents/MacOS/Karabiner-VirtualHIDDevice-Manager activate
-      '';
-      serviceConfig.Label = "org.nixos.start_karabiner_daemons";
-      serviceConfig.RunAtLoad = true;
-      serviceConfig.StandardErrorPath = /tmp/karabiner-daemons.err;
-      serviceConfig.StandardOutPath = /tmp/karabiner-daemons.out;
-    };
-
-    launchd.daemons.Karabiner-DriverKit-VirtualHIDDeviceDaemon = {
-      command = "'/Library/Application Support/org.pqrs/Karabiner-DriverKit-VirtualHIDDevice/Applications/Karabiner-VirtualHIDDevice-Daemon.app/Contents/MacOS/Karabiner-VirtualHIDDevice-Daemon'";
-
-      serviceConfig.ProcessType = "Interactive";
-      serviceConfig.Label = "org.pqrs.Karabiner-DriverKit-VirtualHIDDeviceDaemon";
-      serviceConfig.KeepAlive = true;
-      serviceConfig.StandardErrorPath = /tmp/karabiner-driverkit.err;
-      serviceConfig.StandardOutPath = /tmp/karabiner-driverkit.out;
-    };
-
-    # # Normally karabiner_console_user_server calls activate on the manager but
-    # # because we use a custom location we need to call activate manually.
-    # launchd.user.agents.activate_karabiner_system_ext = {
-    #   serviceConfig.ProgramArguments = [
-    #     "${parentAppDir}/.Karabiner-VirtualHIDDevice-Manager.app/Contents/MacOS/Karabiner-VirtualHIDDevice-Manager"
-    #     "activate"
-    #   ];
-    #   serviceConfig.RunAtLoad = true;
-    # };
-    # environment.etc."sudoers.d/kanata".source = pkgs.runCommand "sudoers-kanata" { } ''
-    #   KANATA_BIN="${pkgs.kanata}/bin/kanata"
-    #   SHASUM=$(sha256sum "$KANATA_BIN" | cut -d' ' -f1)
-    #   cat <<EOF >"$out"
-    #   %admin ALL=(root) NOPASSWD: sha256:$SHASUM $KANATA_BIN
-    #   EOF
-    # '';
-
     # NOTE: this is quite ugly... Since this part is *not* handled by Nix.
-    launchd.daemons.karabiner = {
+    launchd.daemons.karabiner_driver = {
       serviceConfig = {
         ProgramArguments = [
           "/Library/Application Support/org.pqrs/Karabiner-DriverKit-VirtualHIDDevice/Applications/Karabiner-VirtualHIDDevice-Daemon.app/Contents/MacOS/Karabiner-VirtualHIDDevice-Daemon"
         ];
+        RunAtLoad = true;
         KeepAlive = true;
-        ProcessType = "Interactive";
       };
     };
 
-    # TODO: make this a user agent.
-    launchd.daemons.kanata = {
-      serviceConfig = {
-        ProgramArguments = [
-          "${pkgs.kanata}/bin/kanata"
-          "-c"
-          "${configFile}"
-        ];
-        # NOTE: this allows ctrl + space + esc to be used as an escape hatch.
-        KeepAlive = false;
-        RunAtLoad = true;
-      };
-    };
+    # # TODO: make this a user agent.
+    # launchd.daemons.kanata = {
+    #   serviceConfig = {
+    #     ProgramArguments = [
+    #       "${pkgs.kanata}/bin/kanata"
+    #       "-c"
+    #       "${configFile}"
+    #     ];
+    #     # NOTE: this allows ctrl + space + esc to be used as an escape hatch.
+    #     KeepAlive = false;
+    #     RunAtLoad = true;
+    #   };
+    # };
   };
 }
