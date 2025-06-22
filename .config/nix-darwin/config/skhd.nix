00374995e1d94dc5ff73a017dc619edb99c52570
Basile Dura <basile@bdura.me>
2025-06-22 13:32:00 +0200

    feat: add skhd config...

--- .config/nix-darwin/config/skhd.nix
+++ .config/nix-darwin/config/skhd.nix
@@ -4,10 +4,11 @@
 }:
 {
   services.skhd = {
-    enable = true;
+    enable = false;
     package = pkgs.skhd;
-    skhdConfig = [
-      "control + option + shift - return : open -n '/Applications/Nix Trampolines/Ghostty.app'"
-    ];
+    skhdConfig = ''
+      control + option + shift - return : open -n '/Applications/Nix Trampolines/Ghostty.app'
+      cmd + shift - return : wezterm cli spawn --new-window || open '/Applications/Nix Trampolines/WezTerm.app'
+    '';
   };
 }
