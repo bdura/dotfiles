07b50ff6912759f79b4504dbb8239912cd458bf7
Basile Dura <basile@bdura.me>
2025-08-31 23:25:09 +0200

    fix: add user password

--- nix/hosts/nixbtw/users.nix
+++ nix/hosts/nixbtw/users.nix
@@ -9,6 +9,7 @@ in
       homeMode = "755";
       isNormalUser = true;
       description = "${gitUsername}";
+      hashedPassword = "$y$j9T$SZ7yFA8sS4Lvg2pSZwzTi/$s0ungptwpl3KH64MmRXeOrsM9e/u785ngGD1r.5QY03";
       extraGroups = [
         "networkmanager"
         "wheel"
