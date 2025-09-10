8f854fce4070daccdfe9baa79e0858bed4b93acf
Basile Dura <basile@bdura.me>
2025-09-10 17:14:13 +0200

    fix: watchexec with pytest

--- .config/fish/conf.d/abbreviations.fish
+++ .config/fish/conf.d/abbreviations.fish
@@ -13,5 +13,5 @@ abbr -a -- de 'direnv edit'
 # Execute
 abbr -a -- watchc 'watchexec -c -w crates/ -w Cargo.lock cargo t'
 abbr -a -- watchm 'watchexec -c -w crates/ -w Cargo.lock maturin develop --uv -r'
-abbr -a -- watcht 'watchexec -c --no-vcs-ignore -w src/ -w uv.lock -w tests/ pytest'
+abbr -a -- watcht 'watchexec -c --no-vcs-ignore -w src/ -w uv.lock -w tests/ --no-process-group pytest'
 abbr -a -- watchs 'watchexec -c --no-vcs-ignore -w src/ -w uv.lock stubtest --allowlist .stubtest-allowlist'
