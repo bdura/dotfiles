9d949c904ffe2826a092de86da7ae402b135d05e
Basile Dura <basile@bdura.me>
2025-09-10 17:14:29 +0200

    feat: colorise --help with bat

--- .config/fish/conf.d/abbreviations.fish
+++ .config/fish/conf.d/abbreviations.fish
@@ -15,3 +15,6 @@ abbr -a -- watchc 'watchexec -c -w crates/ -w Cargo.lock cargo t'
 abbr -a -- watchm 'watchexec -c -w crates/ -w Cargo.lock maturin develop --uv -r'
 abbr -a -- watcht 'watchexec -c --no-vcs-ignore -w src/ -w uv.lock -w tests/ --no-process-group pytest'
 abbr -a -- watchs 'watchexec -c --no-vcs-ignore -w src/ -w uv.lock stubtest --allowlist .stubtest-allowlist'
+
+# Bat colorisation
+abbr -a --position anywhere -- --help '--help | bat -p -l help'
