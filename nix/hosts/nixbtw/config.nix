e47aff09fcbb9cb1e6e26afeeba3695e2ee3d739
Basile Dura <basile@bdura.me>
2025-09-16 11:03:35 +0200

    feat: enable printing

--- nix/hosts/nixbtw/config.nix
+++ nix/hosts/nixbtw/config.nix
@@ -242,6 +242,18 @@ in
         };
       };
     };
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
+    printing = {
+      enable = true;
+      drivers = with pkgs; [
+        cups-filters
+        cups-browsed
+      ];
+    };
   };
 
   # Bluetooth Support
