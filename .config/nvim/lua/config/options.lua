8f0718c7d69bff57d9af64fda87b45e4fefe968f
Basile Dura <basile@bdura.me>
2025-12-18 08:26:42 +0100

    wip: add confit-lsp configuration

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -6,3 +6,21 @@ local set = vim.opt
 
 set.list = true
 set.listchars = { tab = '» ', trail = '·', nbsp = '␣', lead = '·', multispace = '·' }
+
+vim.lsp.config['confit-lsp'] = {
+  -- Command and arguments to start the server.
+  cmd = { 'confit-lsp' },
+  -- Filetypes to automatically attach to.
+  filetypes = { 'toml' },
+  -- Sets the "workspace" to the directory where any of these files is found.
+  -- Files that share a root directory will reuse the LSP server connection.
+  -- Nested lists indicate equal priority, see |vim.lsp.Config|.
+  root_markers = { { 'pyproject.toml' }, '.git' },
+  -- Specific settings to send to the server. The schema is server-defined.
+  -- Example: https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
+}
+
+vim.highlight.priorities.semantic_tokens = 95
+
+-- vim.lsp.enable('ty')
+-- vim.diagnostic.config({ virtual_lines = { current_line = true } })
