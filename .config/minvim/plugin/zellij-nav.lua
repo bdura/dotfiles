e940a8a0503894825d215f28c529e5564d1e543d
Basile Dura <basile@bdura.me>
2026-04-26 10:02:25 +0200

    fix: add i/v modes to zellij-nav

--- .config/minvim/plugin/zellij-nav.lua
+++ .config/minvim/plugin/zellij-nav.lua
@@ -1,12 +1,10 @@
-vim.pack.add({
-  'https://github.com/swaits/zellij-nav.nvim',
-})
+vim.pack.add({ 'https://github.com/swaits/zellij-nav.nvim' })
 
 require('zellij-nav').setup()
 
 local map = vim.keymap.set
 
-map('n', '<c-h>', '<cmd>ZellijNavigateLeft<cr>', { silent = true, desc = 'navigate left or tab' })
-map('n', '<c-j>', '<cmd>ZellijNavigateDown<cr>', { silent = true, desc = 'navigate down' })
-map('n', '<c-k>', '<cmd>ZellijNavigateUp<cr>', { silent = true, desc = 'navigate up' })
-map('n', '<c-l>', '<cmd>ZellijNavigateRight<cr>', { silent = true, desc = 'navigate right or tab' })
+map({ 'n', 'i', 'v' }, '<c-h>', '<cmd>ZellijNavigateLeft<cr>', { silent = true, desc = 'Navigate left' })
+map({ 'n', 'i', 'v' }, '<c-j>', '<cmd>ZellijNavigateDown<cr>', { silent = true, desc = 'Navigate down' })
+map({ 'n', 'i', 'v' }, '<c-k>', '<cmd>ZellijNavigateUp<cr>', { silent = true, desc = 'Navigate up' })
+map({ 'n', 'i', 'v' }, '<c-l>', '<cmd>ZellijNavigateRight<cr>', { silent = true, desc = 'Navigate right' })
