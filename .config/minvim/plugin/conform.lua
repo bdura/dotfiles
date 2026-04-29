42917cd1a72379110191062f29421693dbcd0aa0
Basile Dura <basile@bdura.me>
2026-04-30 01:29:52 +0200

    wip: clang-format for openscad

--- .config/minvim/plugin/conform.lua
+++ .config/minvim/plugin/conform.lua
@@ -45,6 +45,7 @@ require('conform').setup({
     markdown = { 'rumdl', 'injected' },
     kdl = { 'kdlfmt' },
     nix = { 'alejandra' },
+    openscad = { 'clang-format' },
   },
   default_format_opts = {
     lsp_format = 'fallback',
