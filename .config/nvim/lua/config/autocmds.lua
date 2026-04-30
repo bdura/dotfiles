dd416cf865e1e5b48866ed0651dc2ae0d6d404f5
Basile Dura <basile@bdura.me>
2026-04-30 09:11:02 +0200

    refactor: switch from lazy to bare nvim

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,8 +1,13 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
---
--- Add any additional autocmds here
--- with `vim.api.nvim_create_autocmd`
---
--- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+local highlight_group = augroup('Highlight', { clear = true })
+
+-- Highlight when yanking (copying) text
+autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = highlight_group,
+  callback = function()
+    vim.hl.on_yank({ timeout = 170 })
+  end,
+})
--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,36 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+-- Package management
+map('n', '<leader>Pu', '<cmd>lua vim.pack.update()<CR>')
+
+-- "Hot" reloading
+map('n', '<leader><leader>x', '<cmd>source %<cr>')
+map('n', '<leader>x', ':.lua<cr>')
+map('v', '<leader>x', ':lua<cr>')
+
+map({ 'i', 'x', 'n', 's' }, '<C-s>', '<cmd>w<cr><esc>', { desc = 'Save File' })
+
+-- Exit match highlighting on Esc
+map('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Avoid copying on paste
+map('v', 'p', '"_dP', opts)
+
+-- better indenting
+map({ 'n', 'v' }, '>', '>gv')
+map({ 'n', 'v' }, '<', '<gv')
+
+-- Smart j/k: moves by visual lines when no count, real lines with count
+map({ 'n', 'x' }, 'j', "v:count == 0 ? 'gj' : 'j'", { desc = 'Down', expr = true, silent = true })
+map({ 'n', 'x' }, '<Down>', "v:count == 0 ? 'gj' : 'j'", { desc = 'Down', expr = true, silent = true })
+map({ 'n', 'x' }, 'k', "v:count == 0 ? 'gk' : 'k'", { desc = 'Up', expr = true, silent = true })
+map({ 'n', 'x' }, '<Up>', "v:count == 0 ? 'gk' : 'k'", { desc = 'Up', expr = true, silent = true })
+
+-- Move lines up/down
+map('n', '<A-j>', "<cmd>execute 'move .+' . v:count1<cr>==", { desc = 'Move Down' })
+map('n', '<A-k>', "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = 'Move Up' })
+map('i', '<A-j>', '<esc><cmd>m .+1<cr>==gi', { desc = 'Move Down' })
+map('i', '<A-k>', '<esc><cmd>m .-2<cr>==gi', { desc = 'Move Up' })
+map('v', '<A-j>', ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = 'Move Down' })
+map('v', '<A-k>', ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = 'Move Up' })
