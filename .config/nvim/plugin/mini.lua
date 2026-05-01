2c7e99f76473cf3ee394f835f48587f7a1ff5444
Basile Dura <basile@bdura.me>
2026-05-01 16:47:03 +0200

    feat(nvim): add mini.pairs

--- .config/nvim/plugin/mini.lua
+++ .config/nvim/plugin/mini.lua
@@ -2,16 +2,29 @@ vim.pack.add({
   'https://github.com/nvim-mini/mini.nvim',
 })
 
+-- # Icons
 local icons = require('mini.icons')
-
 -- WESL icons
 local glyph = '󰬄'
 local hl = 'MiniIconsBlue'
-
 icons.setup({
-  filetype  = { wesl = { glyph = glyph, hl = hl } },
+  filetype = { wesl = { glyph = glyph, hl = hl } },
   extension = { wesl = { glyph = glyph, hl = hl } },
 })
--- icons.mock_nvim_web_devicons()
 
+-- # Status line
 require('mini.statusline').setup()
+
+-- # Auto-pairing
+require('mini.pairs').setup({
+  modes = { insert = true, command = true, terminal = false },
+  -- skip autopair when next character is one of these
+  skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+  -- skip autopair when the cursor is inside these treesitter nodes
+  skip_ts = { 'string' },
+  -- skip autopair when next character is closing pair
+  -- and there are more closing pairs than opening pairs
+  skip_unbalanced = true,
+  -- better deal with markdown code blocks
+  markdown = true,
+})
