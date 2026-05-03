0859e0feb91ef331d2ccc33d3ac5110ae4811a6b
Basile Dura <basile@bdura.me>
2026-05-03 10:19:27 +0200

    fix: update password, use password file instead of hashed password

--- nix/hosts/nixbtw/users.nix
+++ nix/hosts/nixbtw/users.nix
@@ -1,15 +1,20 @@
-{ pkgs, username, ... }:
-
-let
-  inherit (import ./variables.nix) gitUsername;
-in
 {
+  pkgs,
+  username,
+  ...
+}: let
+  inherit (import ./variables.nix) gitUsername;
+in {
   users.users = {
     "${username}" = {
       homeMode = "755";
       isNormalUser = true;
       description = "${gitUsername}";
-      hashedPassword = "$y$j9T$SZ7yFA8sS4Lvg2pSZwzTi/$s0ungptwpl3KH64MmRXeOrsM9e/u785ngGD1r.5QY03";
+      # NOTE: on first login, use this password.
+      # Then, immediately create a file containing the hash for the *actual* password
+      # (see below)
+      initialHashedPassword = "$y$j9T$u9Vi2s3/fJru5y48CgYCl/$ZXrpSMA72ziaeJL0ZzqrLFQl4kTs1ScI5YvRs8sfGeA";
+      hashedPasswordFile = "/etc/secrets/basile-password-hash";
       extraGroups = [
         "networkmanager"
         "wheel"
@@ -20,7 +25,7 @@ in
       ];
       shell = pkgs.fish;
       ignoreShellProgramCheck = true;
-      packages = [ ];
+      packages = [];
     };
   };
 }
