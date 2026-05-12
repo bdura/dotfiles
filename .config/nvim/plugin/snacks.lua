bbf0ad506e52642bf64ea824fdcb4efad7053dee
Basile Dura <basile@bdura.me>
2026-05-12 23:21:06 +0200

    fix: remove [[/]] keybinds from terminal mode

--- .config/nvim/plugin/snacks.lua
+++ .config/nvim/plugin/snacks.lua
@@ -270,8 +270,8 @@ local keymaps = {
   { "<leader>gg", function() Snacks.lazygit() end,                                        desc = "Lazygit" },
   { "<leader>un", function() Snacks.notifier.hide() end,                                  desc = "Dismiss All Notifications" },
   { "<c-_>",      function() Snacks.terminal() end,                                       desc = "which_key_ignore" },
-  { "]]",         function() Snacks.words.jump(vim.v.count1) end,                         desc = "Next Reference",           mode = { "n", "t" } },
-  { "[[",         function() Snacks.words.jump(-vim.v.count1) end,                        desc = "Prev Reference",           mode = { "n", "t" } },
+  { "]]",         function() Snacks.words.jump(vim.v.count1) end,                         desc = "Next Reference" },
+  { "[[",         function() Snacks.words.jump(-vim.v.count1) end,                        desc = "Prev Reference" },
   {
     "<leader>N",
     desc = "Neovim News",
