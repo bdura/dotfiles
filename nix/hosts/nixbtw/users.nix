0f55d99e07e0e807ad17008d960104597877babb
Basile Dura <basile@bdura.me>
2026-05-19 15:57:06 +0200

    feat: remove bootstrap password

--- nix/hosts/nixbtw/users.nix
+++ nix/hosts/nixbtw/users.nix
@@ -11,7 +11,7 @@
       # NOTE: on first login, use this password.
       # Then, immediately create a file containing the hash for the *actual* password
       # (see below)
-      initialHashedPassword = "$y$j9T$u9Vi2s3/fJru5y48CgYCl/$ZXrpSMA72ziaeJL0ZzqrLFQl4kTs1ScI5YvRs8sfGeA";
+      # initialHashedPassword = "$y$j9T$u9Vi2s3/fJru5y48CgYCl/$ZXrpSMA72ziaeJL0ZzqrLFQl4kTs1ScI5YvRs8sfGeA";
       hashedPasswordFile = "/etc/secrets/basile-password-hash";
       extraGroups = [
         "networkmanager"
