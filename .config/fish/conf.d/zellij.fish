094fac3a5bd38540a9f681e9c18054770fbb1655
Basile Dura <basile@bdura.me>
2026-07-22 14:41:36 +0200

    feat(fish): update tab name automatically

---
+++ .config/fish/conf.d/zellij.fish
@@ -0,0 +1,43 @@
+# Zellij tab name auto-updater
+#
+# Automatically updates Zellij tab names with the current directory path.
+# Includes git repository information when available.
+# Uses fish's on-variable hook to trigger updates on directory changes.
+
+# Update Zellij tab name with the current directory
+function _zellij_update_tabname
+    if set -q ZELLIJ
+        set current_dir $PWD
+        if test $current_dir = $HOME
+            set tab_name "~"
+        else
+            set tab_name (basename $current_dir)
+        end
+
+        if fish_git_prompt >/dev/null
+            # we are in a git repo
+
+            # if we are in a git superproject, use the superproject name
+            # otherwise, use the toplevel repo name
+            set git_root (git rev-parse --show-superproject-working-tree)
+            if test -z $git_root
+                set git_root (git rev-parse --show-toplevel)
+            end
+
+            #  if we are in a subdirectory of the git root, use the relative path
+            if test (string lower "$git_root") != (string lower "$current_dir")
+                set tab_name (basename $git_root)/(basename $current_dir)
+            end
+        end
+
+        nohup zellij action rename-tab $tab_name >/dev/null 2>&1
+    end
+end
+
+# Auto update tab name on directory change
+function __auto_zellij_update_tabname --on-variable PWD --description "Update zellij tab name on directory change"
+    _zellij_update_tabname
+end
+
+# Run on zellij start-up, without waiting for a directory change
+_zellij_update_tabname
